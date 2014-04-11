68025802c4fe218850ac2b650e8ba8a37f9f89ce
Sean <Sean_Lu@asus.com>
2014-04-11 18:57:34 +0800

    [ME176C][Display][NA][Update] Turn off dpst for factory test
    Change-Id: I6f1c5e849bcd26f41ccbad8a47ae03ea2ee4ceee

--- drivers/gpu/drm/i915/i915_dpst.c
+++ drivers/gpu/drm/i915/i915_dpst.c
@@ -286,9 +286,17 @@ i915_dpst_init(struct drm_device *dev,
 			sean_debug("%s:----sean test----%d\n", __func__,__LINE__);
 		}
 	}
+#else //ME176C
+
+#if IS_ENABLED(CONFIG_FACTORY_ITEMS) //only factory version
+	//i915_dpst_enable_hist_interrupt(dev);
+	i915_dpst_disable_hist_interrupt(dev);
+	sean_debug("%s:----sean test----%d\n", __func__,__LINE__);
 #else
 	i915_dpst_enable_hist_interrupt(dev);
 	sean_debug("%s:----sean test----%d\n", __func__,__LINE__);
+#endif
+
 #endif
 
 
